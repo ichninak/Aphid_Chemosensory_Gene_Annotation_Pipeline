5c503268c48f7d1c4bd7d980525d3d9521a61c45
ishaac chninak <ichninak@REN-1349-B133.inra.local>
2025-07-30 10:19:20 +0200

    sbatch

---
+++ run_nextflow.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=nextflow_job
+#SBATCH --cpus-per-task=50       
+#SBATCH --mem=100G                
+#SBATCH --time=24:00:00          
+#SBATCH --output=nextflow_%j.out
+#SBATCH --error=nextflow_%j.err
+
+# Load necessary modules
+conda activate aphid-nf
+
+# Variables
+
+echo "Start at $(date)"
+
+
+# Launch the pipeline
+nextflow run main.nf -OR true -resume
+
+echo "end at $(date)"
